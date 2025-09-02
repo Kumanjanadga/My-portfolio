272e0f4dd40a870b2f9dbd83eaca71d9cc2719bf
Akila Kumanjana <akilakumanjanag@gmail.com>
2025-09-02 16:08:08 +0530

    Fix image path in Projects component for correct background image loading

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -22,7 +22,7 @@ export default function Projects() {
         }
       ],
       //image: "./AskHire.png",
-      image: "bg-[url('public/AskHire.png')] bg-cover bg-center",
+      image: "bg-[url('/AskHire.png')] bg-cover bg-center",
       type: "Academic Project",
       demoUrl: "" // Add demo URL if available
     },
